c4a5f2b874f3b71ad7961ef0045f81c48895f36e
davidkuperman <kuper_7@hotmail.com>
2014-05-19 22:34:51 -0500

    user created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Ej8::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/about"
-  get "static_pages/signup"
+  match '/about', to: 'static_pages#about', via: 'get'
+  match '/signup', to: 'users#new', via: 'get'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
